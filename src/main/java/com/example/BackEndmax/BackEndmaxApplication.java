60eda96f3285ea6a9b56c0647e73d6640739a0de
MHDINFAS <mohammedinfas2129@gmail.com>
2025-01-09 14:11:25 +0530

    initial commit

---
+++ src/main/java/com/example/BackEndmax/BackEndmaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndmax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndmaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndmaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackEndmax/BackEndmaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndmax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndmaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
